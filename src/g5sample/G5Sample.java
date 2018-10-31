d71e6c94daa3f3acf346cf9075b5bf2fefb31cff
Tarc <Tarc@PC178>
2018-10-31 15:02:10 +0800

    first version

--- src/g5sample/G5Sample.java
+++ src/g5sample/G5Sample.java
@@ -16,6 +16,7 @@ public class G5Sample {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world 2");
     }
     
 }
